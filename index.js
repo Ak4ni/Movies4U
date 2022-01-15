f6935a0726e57f189bd8416a854a976815b607ec
Andres Rivera <89215774+Ak4ni@users.noreply.github.com>
2022-01-15 10:19:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -133,11 +133,12 @@ app.get("/user", passport.authenticate('jwt', { session: false }), function (req
   Email: String,
   Birthday: Date
 }*/
-app.post('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.post('/users', (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
+        return res.status(400).send(req.body.Username + ' already exists');
       } else {
         Users
           .create({
@@ -146,11 +147,11 @@ app.post('/users', passport.authenticate('jwt', { session: false }), (req, res) 
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
       }
     })
     .catch((error) => {
