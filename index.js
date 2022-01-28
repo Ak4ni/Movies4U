156bfb1dcb3a2d2edf55981ae1f1d00b379b89a1
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-01-28 17:57:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -78,17 +78,16 @@ app.get('/documentation', (req, res) => {
 });
 
 //query db for movies
-app.get("/movies",passport.authenticate('jwt', { session: false }), (req, res) => {
-  Movies4Udb.find()
-  .then((movies) => {
-    res.status(201).json(movies);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
-  });
+app.get("/movies", function (req, res) {
+  Movies.find()
+    .then(function (movies) {
+      res.status(201).json(movies);
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
-
 
 
 //Get data specific by Title
